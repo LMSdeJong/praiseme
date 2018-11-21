6ceda75c1d3265f40336dc647a71d85e2f6ecc46
Laura de Jong <l.dejong@deakin.edu.au>
2018-11-21 15:22:39 +1100

    initialise the git resposeitory, the first push of the praiseme package

---
+++ R/praise.R
@@ -0,0 +1,17 @@
+#' @title Praise someone
+#' @description Let's dole out praise
+#' @param name specifies recipient of praise
+#' @param punctuation Specifies tone of praise
+#'
+#' @examples
+#' praise("Nick")
+#' praise("Holly")
+#'
+#' @export
+
+praise <- function(name="Laura", punction="!"){
+
+  paste0("you're the best, ", name, punctuation)
+
+}
+
